67f194c71fcdbc8329876f6242e48cbec2e56c16
Carlos vasquez <116930495+CVV94@users.noreply.github.com>
2023-12-04 14:25:28 -0300

    Se Guarda Cambios de settings

--- core/settings.py
+++ core/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pasteleriaelielOF',
+        'NAME': 'pasteleriaeliel',
         'USER': 'root',
         'PASSWORD': ''
     }
